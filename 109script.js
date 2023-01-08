677677a96ae114ff4c77a7a97d7a4e75f9337f44
TTSCool <vietdainhan1@gmail.com>
2023-01-08 20:34:22 +0700

    109

---
+++ 109script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian','Pizzeria','Vegetarian','Organic'], 
+    starterMenu: ['Focaccia','Bruschetta','Garlic','Bread','Caprese Salad'],
+    mainMenu: ['Pizza','Pasta','Risotto'],
+
+    order: function  (){
+
+    }
+};
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x,y,z] = arr;
+console.log(x,y,z);
+console.log(arr);
+
+let [main, ,secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main] ;
+console.log(main, secondary);
